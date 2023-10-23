ec5539ade6094e0cce261ca0ae0c6dc78f94836b
x5tr <5tr@duck.com>
2023-10-23 16:52:28 +0900

    staged

--- index.js
+++ index.js
@@ -17,19 +17,19 @@ app.get('/', (req, res) => {
 app.listen(3000)
 
 function getCommands() {
-	fs.rmSync('commands', { recursive: true, force: true });
+	fs.rmSync('cmds', { recursive: true, force: true });
 	shell.exec(`git clone ${process.env.REPOSITORY} Athens`);
-	shell.mv('-f', 'Athens/commands', 'commands');
+	shell.mv('-f', 'Athens/cmds', 'cmds');
 	shell.rm('-rf', 'Athens');
 }
 
 function setNewCommandsCollection() {
 	client.commands = new Collection();
 
-	const commandFiles = fs.readdirSync('commands').filter(file => file.endsWith('.js'));
+	const commandFiles = fs.readdirSync('cmds').filter(file => file.endsWith('.js'));
 
 	for (const file of commandFiles) {
-		const command = require(`commands/${file}`);
+		const command = require(`./cmds/${file}`);
 		client.commands.set(command.data.name, command);
 	}
 }
@@ -51,10 +51,10 @@ client.on('interactionCreate', async interaction => {
 		getCommands();
 		
 		const commands = [];
-		const commandFiles = fs.readdirSync('commands').filter(file => file.endsWith('.js'));
+		const commandFiles = fs.readdirSync('cmds').filter(file => file.endsWith('.js'));
 
 		for (const file of commandFiles) {
-			const newCommand = require(`commands/${file}`);
+			const newCommand = require(`./cmds/${file}`);
 			commands.push(newCommand.data.toJSON());
 		}
 
@@ -87,6 +87,48 @@ client.on('interactionCreate', async interaction => {
 	if (!command) return;
 
 	try {
+		if (command.requiresVote) {
+			const voteMessage = await interaction.reply({
+				content: `Vote to ${command.name} ${interaction.options.getUser('user').username}`,
+				components: [
+				  {
+					type: 'ACTION_ROW',
+					components: [
+					  { type: 'BUTTON', style: 'SUCCESS', label: 'Agree', customId: `${command.name}_agree` },
+					  { type: 'BUTTON', style: 'DANGER', label: 'Disagree', customId:`${command.name}_disagree` },
+					],
+				  },
+				],
+				fetchReply:true,
+			  });
+			
+			  const filter = (buttonInteraction) =>
+				buttonInteraction.customId.startsWith(`${command.name}_`) && buttonInteraction.user.id !== interaction.user.id;
+				
+			   const collector = voteMessage.createMessageComponentCollector({ filter, time :60000 });
+			
+			   let agreeCount = 0;
+			   let disagreeCount = 0;
+			
+			  collector.on('collect', (buttonInteraction) => {
+				if (buttonInteraction.customId === `${command.name}_agree`) {
+				  agreeCount++;
+				} else if (buttonInteraction.customId === `${command.name}_disagree`) {
+				  disagreeCount++;
+				}
+			  });
+
+			  collector.on('end', async () => {
+				const totalVotes = agreeCount + disagreeCount;
+				
+				if (agreeCount >= Math.ceil(totalVotes / 2)) {
+				  await interaction.followUp(`The vote to ${commandName} ${interaction.options.getUser('user').username} passed.`);
+				  await command.execute(interaction);
+				} else {
+				   await interaction.followUp(`The vote to ${commandName} ${interaction.options.getUser('user').username} failed.`);
+				 }
+			  });
+		}
 		await command.execute(interaction);
 	} catch (error) {
 		console.error(error);
